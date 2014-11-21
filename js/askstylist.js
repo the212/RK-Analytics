5e8c9911f33c5f5285f22755671b3b0262cec91c
Sebastian Soler <sebastian.a.soler@gmail.com>
2014-11-21 11:38:49 -0500

    updated analtics data fetch to live data

--- js/askstylist.js
+++ js/askstylist.js
@@ -10,46 +10,39 @@ $(function() {
 	  client.draw(query, document.getElementById("total_ask"), {
 	    // Custom configuration here
 	  });
-	});
 
-	Keen.ready(function(){
-	  var query = new Keen.Query("count", {
+	  var query2 = new Keen.Query("count", {
 	    eventCollection: "ask_submit",
 	    timeframe: "this_week"
 	  });
-	  client.draw(query, document.getElementById("week_ask"), {
+	  client.draw(query2, document.getElementById("week_ask"), {
 	    // Custom configuration here
 	  });
-	});
 
-	Keen.ready(function(){
-	  var query = new Keen.Query("count", {
+	  var query3 = new Keen.Query("count", {
 	    eventCollection: "ask_submit",
 	    groupBy: "looking"
 	  });
-	  client.draw(query, document.getElementById("looking"), {
+	  client.draw(query3, document.getElementById("looking"), {
 	    // Custom configuration here
 	  });
-	});
 
-	Keen.ready(function(){
-	  var query = new Keen.Query("count", {
+	  var query4 = new Keen.Query("count", {
 	    eventCollection: "ask_submit",
 	    groupBy: "time"
 	  });
-	  client.draw(query, document.getElementById("time"), {
+	  client.draw(query4, document.getElementById("time"), {
 	    // Custom configuration here
 	  });
-	});
 
-	Keen.ready(function(){
-	  var query = new Keen.Query("count", {
+	  var query5 = new Keen.Query("count", {
 	    eventCollection: "ask_submit",
 	    groupBy: "day_sent"
 	  });
-	  client.draw(query, document.getElementById("day_sent"), {
+	  client.draw(query5, document.getElementById("day_sent"), {
 	    // Custom configuration here
 	  });
+
 	});
 
 });
